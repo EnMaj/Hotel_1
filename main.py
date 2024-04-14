69257f83d2cab3795711913a30afee4366fe7f3b
TC <yakimova.tonya@gmal.com>
2024-04-14 21:44:04 +0700

    new

--- main.py
+++ main.py
@@ -29,9 +29,7 @@ class HotelRoom:
         }
 
         if sort in types and comfort in ratios:
-            return types[sort] * ratios[comfort]
-
-        return 'ERROR'
+            return float(types[sort] * ratios[comfort])
 
     @staticmethod
     def dietary(food_cost):
@@ -46,7 +44,7 @@ class HotelRoom:
 
     def booking(self, busy_date: str, days):
         date = int(busy_date[:2])
-        self.busy.append([date, date + days])
+        self.busy.append([date, date + days - 1])
 
 
 if __name__ == '__main__':
@@ -72,14 +70,27 @@ if __name__ == '__main__':
         missed_profit = 0
         for info in book:
             booking_date, s_name, f_name, surname, num_people, busy_date, days, max_money = info.split()
+            num_people = float(num_people)
+            days = float(days)
+            max_money = float(max_money)
+
             if int(booking_date[:2]) != date_now:
                 for room in hotel:
+                    # print(room.busy)
                     for date in room.busy:
                         if date[0] <= date_now <= date[1]:
                             busy_room_number += 1
                             categories_busy[room.tp] += 1
                 date_now = int(booking_date[:2])
-                print(f'')
+                print(f'{ru_local.NUMBER_BUSY_ROOM} {busy_room_number}\n'
+                      f'{ru_local.NUMBER_FREE_ROOM} {len(hotel) - busy_room_number}\n'
+                      f'{ru_local.NUMBER_ONE_PERSON} {round(categories_busy[ru_local.ONE_PERSON]* 100 / 9, 2)}\n'
+                      f'{ru_local.NUMBER_TWO_PERSONS} {round(categories_busy[ru_local.TWO_PERSONS] * 100 / 6, 2)}\n'
+                      f'{ru_local.NUMBER_JUNIOR_SUITE} {round(categories_busy[ru_local.JUNIOR_SUITE] * 100 / 5, 2)}\n'
+                      f'{ru_local.NUMBER_LUXE} {round(categories_busy[ru_local.LUXE] * 100 / 4, 2)}\n'
+                      f'{ru_local.HOTEL_BUSY} {round(busy_room_number*100/len(hotel),2)}\n'
+                      f'{ru_local.PROFIT} {profit}\n'
+                      f'{ru_local.MISSED_PROFIT} {missed_profit}\n')
                 busy_room_number = 0
                 categories_busy = {
                     ru_local.ONE_PERSON: 0,
@@ -89,40 +100,48 @@ if __name__ == '__main__':
                 }
                 profit = 0
                 missed_profit = 0
+
             if int(num_people) > 6:
                 continue
             force_major = random.randint(0, 3)
+            booking_flag = False
             for apartment in hotel:
                 busy_flag = False
                 food_cost = 0
-                if apartment.number == int(num_people) \
+                if int(apartment.capacity) == int(num_people) \
                         and max_money >= apartment.cost(apartment.tp, apartment.comfort):
                     for element in apartment.busy:
-                        if element[0] < busy_date < element[1]:
+                        if element[0] <= float(busy_date[:2]) <= element[1]:
                             busy_flag = True
                     if not busy_flag:
                         food_cost = max_money - apartment.cost(apartment.tp, apartment.comfort)
                         food_price = apartment.dietary(food_cost)
-                        final_price = (0.7 * apartment.cost(apartment.tp, apartment.comfort) \
+                        final_price = (apartment.cost(apartment.tp, apartment.comfort)
                                        + food_price) * num_people
                         if force_major == 0:
                             missed_profit += final_price
-                            continue
+                            break
                         apartment.booking(busy_date, days)
                         profit += final_price
-                if not busy_flag:
-                    if apartment.number == int(num_people) + 1 \
-                            and max_money >= apartment.cost(apartment.tp, apartment.comfort):
+                        booking_flag = True
+                        break
+            if not booking_flag:
+                for apartment in hotel:
+                    busy_flag = False
+                    food_cost = 0
+                    if apartment.capacity == num_people + 1 \
+                            and max_money >= 0.7 * apartment.cost(apartment.tp, apartment.comfort):
                         for element in apartment.busy:
-                            if element[0] < busy_date < element[1]:
+                            if element[0] <= float(busy_date[:2]) <= element[1]:
                                 busy_flag = True
                         if not busy_flag:
                             food_cost = max_money - 0.7 * apartment.cost(apartment.tp, apartment.comfort)
                             food_price = apartment.dietary(food_cost)
-                            final_price = (0.7 * apartment.cost(apartment.tp, apartment.comfort) \
+                            final_price = (0.7 * apartment.cost(apartment.tp, apartment.comfort)
                                            + food_price) * num_people
                             if force_major == 0:
                                 missed_profit += final_price
-                                continue
+                                break
                             apartment.booking(busy_date, days)
                             profit += final_price
+                            break
